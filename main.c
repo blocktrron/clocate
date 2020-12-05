f4049c9fcc2ae9e0771dfbf77cde30ac8bcba195
David Bauer <mail@david-bauer.net>
2020-12-05 15:45:39 +0100

    main: scan all wireless interfaces by default

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <netlink/genl/genl.h>
+#include <net/if.h>
 
 #include "nl80211.h"
 #include "google.h"
@@ -9,35 +10,38 @@
 
 
 static void usage(char *p) {
-	printf("Usage: %s interface provider [apikey]\n", p);
+	printf("Usage: %s provider [apikey]\n", p);
 	printf("Available providers: mozilla google\n");
 }
 
 int main(int argc, char *argv[]) {
 	struct geolocation_result geolocation_result = {};
 	struct scan_results results = {};
+	struct if_results ifresults = {};
 	char *interface;
 	char *provider;
 	char *apikey;
 
-	if (argc < 3) {
+	get_wireless_interfaces(&ifresults);
+
+	if (argc < 2) {
 		usage(argv[0]);
 		exit(1);
 	}
 
-	interface = argv[1];
-	provider = argv[2];
+	provider = argv[1];
 
-	if (argc < 4 && !strcmp("mozilla", provider)) {
+	if (argc < 3 && !strcmp("mozilla", provider)) {
 		apikey = NULL;
-	} else if (argc < 4) {
+	} else if (argc < 3) {
 		usage(argv[0]);
 		exit(1);
 	} else {
-		apikey = argv[3];
+		apikey = argv[2];
 	}
 
-	perform_scan(&results, interface);
+	for (int i = 0; i < ifresults.count; i++)
+		perform_scan(&results, &ifresults.buf[i * IF_NAMESIZE]);
 
 	if (!strcmp("mozilla", provider))
 		locate_mozilla(&results, &geolocation_result, apikey);
